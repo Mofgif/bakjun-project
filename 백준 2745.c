9c5bcb576fcd73e56046a914a782a2ed203b8da2
Mofgif <106219457+Mofgif@users.noreply.github.com>
2024-12-18 08:53:55 +0900

    Add files via upload

---
+++ 백준 2745.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(void) 
+{
+    char N[36] = {0};
+    int B, ans = 0;
+
+    scanf_s("%s %d",N,&B);
+
+    for (int i = 0; i < strlen(N); i++) {
+        if (N[i] >= 'A' && N[i] <= 'Z') {
+            ans += (N[i] - 55) * pow(B,strlen(N) - i - 1);
+        }
+        else {
+            ans += (N[i] - 48) * pow(B,strlen(N) - i - 1);
+        }
+    }
+    printf("%d", ans);
+
+    return 0;
+}
